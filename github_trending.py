ab028ec1c91a504817d4d8589fc90456f7fc099a
Виктор Андрюшенков <buktop@MacBook-Air-Viktor.local>
2018-02-13 23:07:49 +0900

    fixed functions get_trending_repositories, count_issues_amount

--- github_trending.py
+++ github_trending.py
@@ -8,43 +8,33 @@ def get_date_week_ago():
     date_week_ago = date_now - datetime.timedelta(days=days_week)
     return date_week_ago.isoformat()
 
-def downlowd_information_repositories(
+
+def get_trending_repositories(
         date_ago,
+        top_size,
         url_api_github='https://api.github.com/search/repositories',
         sort='stars',
         order='desk'
 ):
     search_params = {
-        'q':"created:>{}".format(date_ago),
+        'q':'created:>{}'.format(date_ago),
         'sort': sort,
         'order': order
     }
     try:
         response = requests.get(url_api_github, params=search_params)
         if response.ok:
-            return response.json()
-        else:
-            return None
+            repositories = response.json()
+            return repositories['items'][:top_size]
     except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
         return None
 
 
-def get_trending_repositories(repositories, top_size):
-    return repositories['items'][:top_size]
-
-
-def get_open_reposetory_issues(repo_full_name):
-    url_api = {'main_url': 'https://api.github.com/repos/', 'add_url': '/issues'}
-    reposetory_issues = url_api['main_url'] + repo_full_name + url_api['add_url']
+def count_issues_amount(repo_full_name):
+    reposetory_issues = 'https://api.github.com/repos/{}/issues'.format(repo_full_name)
     response = requests.get(reposetory_issues)
     if response.ok:
-        return response.json()
-    else:
-        return None
-
-
-def count_issues_amount(reposetory_issues):
-    return len(reposetory_issues)
+        return len(response.json())
 
 
 def print_trend_repository(trend_repository, issues_amount):
@@ -55,17 +45,14 @@ def print_trend_repository(trend_repository, issues_amount):
 
 if __name__ == '__main__':
     date_week_ago = get_date_week_ago()
-    repositories = downlowd_information_repositories(date_week_ago)
-    if repositories:
+    top_size = 20
+    trending_repositories = get_trending_repositories(date_week_ago, top_size)
+    if trending_repositories:
         print('New popular projects on github for the week:')
-        top_size = 20
-        trending_repositories = get_trending_repositories(repositories, top_size)
         for trend_repository in trending_repositories:
-            reposetory_issues = get_open_reposetory_issues(trend_repository['full_name'])
-            if reposetory_issues == None:
-                issues_amount = 'no date'
-            else:
-                issues_amount = count_issues_amount(reposetory_issues)
+            issues_amount = count_issues_amount(trend_repository['full_name'])
+            if issues_amount is None:
+                issues_amount = 'no data'
             print_trend_repository(trend_repository, issues_amount)
     else:
         print('Can not load data.')
